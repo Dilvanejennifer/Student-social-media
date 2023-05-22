c99cd95b8e43874b4083abcab6638f2703ab1760
Dilvanejennifer <dilvane8@gmail.com>
2023-05-22 14:56:54 +0530

    initial comit

---
+++ README.md
@@ -0,0 +1,2 @@
+<img width="959" alt="Screenshot_20230124_201158" src="https://user-images.githubusercontent.com/97978224/214325024-ecd69c9c-6ece-4245-b2ed-735b4fad26d0.png">
+
---
+++ timeTable.js
@@ -0,0 +1,171 @@
+const Sunday =[
+    {   
+        time: '--',
+        roomNumber: '--',
+        subject: 'There is no class for today',
+        type: '--'
+    }
+]
+const Monday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '112',
+        subject: 'MPI',
+        type: 'Pratical'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '207',
+        subject: 'WP',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '207',
+        subject: 'MPI',
+        type: 'Lecture'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '112',
+        subject: 'TOC',
+        type: 'Lecture'
+    },
+    {   
+        time: '04-05 PM',
+        roomNumber: '112',
+        subject: 'TOC',
+        type: 'Lecture'
+    }
+
+]
+const Tuesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '207',
+        subject: 'AJP',
+        type: 'Pratical'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '207',
+        subject: 'AJP',
+        type: 'Pratical'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '112',
+        subject: 'CPDP',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '112',
+        subject: 'MPI',
+        type: 'Lecture'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '112',
+        subject: 'WPI',
+        type: 'Lecture'
+    }
+
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '205',
+        subject: 'DV',
+        type: 'Pratical'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '205',
+        subject: 'DV',
+        type: 'Pratical'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '112',
+        subject: 'MPI',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '112',
+        subject: 'WP',
+        type: 'Lecture'
+    },
+    {   
+        time: '04-05 PM',
+        roomNumber: '112',
+        subject: 'AJP',
+        type: 'Tutorial'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: '207',
+        subject: 'DE',
+        type: 'Pratical'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '207',
+        subject: 'DE',
+        type: 'Practical'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '112',
+        subject: 'AJP',
+        type: 'Lecture'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '112',
+        subject: 'DV',
+        type: 'Lecture'
+    },
+    {   
+        time: '04-05 PM',
+        roomNumber: '112',
+        subject: 'DV',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '12-01 AM',
+        roomNumber: '112',
+        subject: 'TOC',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '112',
+        subject: 'TOC',
+        type: 'Lecture'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: 'CPDP',
+        subject: 'CAP214',
+        type: 'Lecture'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '--',
+        roomNumber: '--',
+        subject:'There is no classes today',
+        type: '--'
+    }
+]
